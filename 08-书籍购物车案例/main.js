a95562cf03f7340760885427375f18b36abd398d
jiangfendous <minghong.jiang@grandchallenge.co.jp>
2021-02-01 16:04:13 +0800

    2021/2/1 add

--- 08-书籍购物车案例/main.js
+++ 08-书籍购物车案例/main.js
@@ -35,10 +35,28 @@ const app = new Vue({
     // getFinalPrice(price) {
     //   return '￥' + price.toFixed(2)
     // }
+    increment(index) {
+      this.books[index].count++
+    },
+    decrement(index) {
+      this.books[index].count--
+    },
+    removeHandle(index) {
+      this.books.splice(index, 1)
+    }
+  },
+  computed: {
+    totalPrice() {
+      let totalPrice = 0;
+      for (let i = 0; i < this.books.length; i++) {
+        totalPrice += this.books[i].price * this.books[i].count
+      }
+      return totalPrice
+    }
   },
   filters: {
     showPrice(price) {
-      return '￥' + price.toFixed(2)
+      return '¥' + price.toFixed(2)
     }
   }
 })
